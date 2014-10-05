05454e098d0076484e06812c6a9e630844d97e9d
Les Orchard <me@lmorchard.com>
2014-10-05 00:21:51 -0400

    Further working pokes at Amazon S3

--- src/javascript/app/login-amazon.js
+++ src/javascript/app/login-amazon.js
@@ -1,29 +1,10 @@
 var $ = require('jquery');
 var S3Ajax = require('S3Ajax');
-
-function getQueryParameters (str) {
-  return (str || document.location.search)
-    .replace(/(^\?)/,'').split("&")
-    .map(function (n) {
-      return n = n.split("="),
-        this[n[0]] = decodeURIComponent(n[1]),
-        this
-    }.bind({}))[0];
-}
+var misc = require('../misc');
+var publishers = require('../publishers');
 
 module.exports = function () {
-  var qparams = getQueryParameters();
-
-  window.onAmazonLoginReady = function() {
-    amazon.Login.setClientId('amzn1.application-oa2-client.c64da1621c67449ab764c4cdf2f99761');
-  };
-
-  (function(d) {
-    var a = d.createElement('script'); a.type = 'text/javascript';
-    a.async = true; a.id = 'amazon-login-sdk';
-    a.src = 'https://api-cdn.amazon.com/sdk/login1.js';
-    d.getElementById('amazon-root').appendChild(a);
-  })(document);
+  var qparams = misc.getQueryParameters();
 
   document.getElementById('LoginWithAmazon').onclick = function() {
     options = { scope : 'profile' };
@@ -37,89 +18,73 @@ module.exports = function () {
 
   if (qparams.access_token) {
 
-    $.ajax({
-      url: 'https://sts.amazonaws.com/?' +
-        'ProviderId=www.amazon.com&DurationSeconds=900&' +
-        'Action=AssumeRoleWithWebIdentity&Version=2011-06-15&' +
-        'RoleSessionName=web-identity-federation&' +
-        'RoleArn=arn:aws:iam::197006402464:role/tootsr-amazon-user-buckets&' +
-        'WebIdentityToken=' + qparams.access_token,
-
-      error: function (xhr, status, err) {
-        console.log(status, err);
-      },
-
-      success: function (dataXML, status, xhr) {
-        var data = xmlToObj(dataXML);
-        var credentials = data.AssumeRoleWithWebIdentityResponse
-          .AssumeRoleWithWebIdentityResult.Credentials;
-
-        console.log(data);
-
-        var user_id = data;
-
-        var s3 = new S3Ajax({
-          key_id: credentials.AccessKeyId,
-          secret_key: credentials.SecretAccessKey,
-          security_token: credentials.SessionToken
-        });
-
-      }
-
-    });
-
+    var profile;
     $.ajax({
       url: 'https://api.amazon.com/user/profile',
       headers: { 'Authorization': 'bearer ' + qparams.access_token },
-      success: function (data, status, xhr) {
-        console.log(data);
-      },
       error: function (xhr, status, err) {
         console.log(status, err);
+      },
+      success: function (data, status, xhr) {
+        profile = data;
+        console.log(profile);
+
+        $.ajax({
+          url: 'https://sts.amazonaws.com/?' +
+            'ProviderId=www.amazon.com&DurationSeconds=900&' +
+            'Action=AssumeRoleWithWebIdentity&Version=2011-06-15&' +
+            'RoleSessionName=web-identity-federation&' +
+            'RoleArn=' + publishers.AmazonS3.ROLE_ARN + '&' +
+            'WebIdentityToken=' + qparams.access_token
+        }).done(function (dataXML, status, xhr) {
+
+          var data = misc.xmlToObj(dataXML);
+          var credentials = data.AssumeRoleWithWebIdentityResponse
+            .AssumeRoleWithWebIdentityResult.Credentials;
+
+          var s3 = new S3Ajax({
+            base_url: 'https://s3.amazonaws.com',
+            key_id: credentials.AccessKeyId,
+            secret_key: credentials.SecretAccessKey,
+            security_token: credentials.SessionToken
+          });
+
+          var prefix = 'users/amazon/' + profile.user_id + '/';
+
+          s3.listKeys(
+            'tootsr.apps.lmorchard.com',
+            {
+              prefix: prefix,
+              delimiter: '/'
+            },
+            function (req, obj) {
+              console.log("LIST SUCCESS");
+              console.log(obj);
+            },
+            function (req, obj) {
+              console.log("ERROR");
+              console.log(obj);
+            }
+          );
+
+          s3.put(
+            'tootsr.apps.lmorchard.com',
+            prefix + 'TEST-' + Date.now() + '.txt',
+            "HELLO WORLD AT " + (new Date()),
+            function (req, obj) {
+              console.log("PUT SUCCESS");
+              console.log(obj);
+            },
+            function (req, obj) {
+              console.log("ERROR");
+              console.log(obj);
+            }
+          );
+
+        });
       }
     });
 
   }
 
 };
-
-// Turn a simple structure of nested XML elements into a JavaScript object.
-//
-// TODO: Handle attributes?
-function xmlToObj (parent, force_lists, path) {
-  var obj = {};
-  var cdata = '';
-  var is_struct = false;
-
-  for(var i=0,node; node=parent.childNodes[i]; i++) {
-    if (3 === node.nodeType) {
-      cdata += node.nodeValue;
-    } else {
-      is_struct = true;
-      var name  = node.nodeName;
-      var cpath = (path) ? path+'.'+name : name;
-      var val   = arguments.callee(node, force_lists, cpath);
-
-      if (!obj[name]) {
-        var do_force_list = false;
-        if (force_lists) {
-          for (var j=0,item; item=force_lists[j]; j++) {
-            if (item === cpath) {
-              do_force_list=true; break;
-            }
-          }
-        }
-        obj[name] = (do_force_list) ? [ val ] : val;
-      } else if (obj[name].length) {
-        // This is a list of values to append this one to the end.
-        obj[name].push(val);
-      } else {
-        // Has been a single value up till now, so convert to list.
-        obj[name] = [ obj[name], val ];
-      }
-    }
-  }
-
-  // If any subnodes were found, return a struct - else return cdata.
-  return (is_struct) ? obj : cdata;
-}
--- src/javascript/publishers/AmazonS3.js
+++ src/javascript/publishers/AmazonS3.js
@@ -5,10 +5,23 @@ module.exports = function (publishers, baseClass, baseProto) {
   var AmazonS3 = publishers.makeConstructor();
 
   _.extend(AmazonS3, baseClass, {
+    CLIENT_ID: 'amzn1.application-oa2-client.c64da1621c67449ab764c4cdf2f99761',
+    ROLE_ARN: 'arn:aws:iam::197006402464:role/tootsr-amazon-user-buckets'
   });
 
   _.extend(AmazonS3.prototype, baseProto(AmazonS3), {
   });
+
+  window.onAmazonLoginReady = function() {
+    amazon.Login.setClientId(AmazonS3.CLIENT_ID);
+  };
+
+  (function(d) {
+    var a = d.createElement('script'); a.type = 'text/javascript';
+    a.async = true; a.id = 'amazon-login-sdk';
+    a.src = 'https://api-cdn.amazon.com/sdk/login1.js';
+    d.getElementById('amazon-root').appendChild(a);
+  })(document);
 
   return AmazonS3;
 };
