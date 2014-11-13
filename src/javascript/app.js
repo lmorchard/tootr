c7b21665ba98bd4a9ef4975f51ca1cafda2e47da
Les Orchard <me@lmorchard.com>
2014-11-12 22:06:44 -0500

    Toggle toot edit/delete on toot click or tap

--- src/javascript/app.js
+++ src/javascript/app.js
@@ -84,11 +84,26 @@ function setup (msg, publisher) {
     });
   });
 
+
   $('button#profileEdit').click(profileEdit);
 
   $('button#profileEditDone').click(profileEditDone);
 
-  $('#entries').delegate('button.entry-delete', 'click', function () {
+  $('#entries').delegate('.h-entry', 'click', function (ev) {
+    var entry = $(this);
+    var footer = entry.find('footer.ui-only');
+    if (footer.length) {
+      footer.remove();
+      return;
+    }
+    footer = $('<footer class="ui-only">');
+    footer.append('<button class="ui-only entry-edit btn btn-success btn-sm">Edit</button>');
+    footer.append('<button class="ui-only entry-edit-done btn btn-primary btn-sm">Done</button>');
+    footer.append('<button class="ui-only entry-delete btn btn-danger btn-sm">Delete</button>');
+    entry.append(footer);
+  });
+
+  $('#entries').delegate('button.entry-delete', 'click', function (ev) {
     var button = $(this);
     var entry = button.parents('.h-entry');
     entry.fadeOut(function () {
@@ -101,9 +116,10 @@ function setup (msg, publisher) {
       undo.find('.panel-body').append(entry);
       saveToots(publisher);
     });
+    ev.stopPropagation();
   });
 
-  $('#entries').delegate('button.entry-undo-delete', 'click', function () {
+  $('#entries').delegate('button.entry-undo-delete', 'click', function (ev) {
     var button = $(this);
     var panel = button.parents('.panel.undo-delete');
     var entry = panel.find('.h-entry');
@@ -114,9 +130,10 @@ function setup (msg, publisher) {
         saveToots(publisher);
       });
     });
+    ev.stopPropagation();
   });
 
-  $('#entries').delegate('button.entry-edit', 'click', function () {
+  $('#entries').delegate('button.entry-edit', 'click', function (ev) {
     var button = $(this);
     var entry = button.parents('.h-entry');
     var field = $('<textarea class="ui-only form-control"></textarea>');
@@ -126,9 +143,10 @@ function setup (msg, publisher) {
     field.insertAfter(content).val(content.html()).change(function () {
       content.html(field.val());
     });
+    ev.stopPropagation();
   });
 
-  $('#entries').delegate('button.entry-edit-done', 'click', function () {
+  $('#entries').delegate('button.entry-edit-done', 'click', function (ev) {
     var button = $(this);
     var entry = button.parents('.h-entry');
     var field = entry.find('textarea');
@@ -138,6 +156,7 @@ function setup (msg, publisher) {
     content.html(field.val());
     saveToots(publisher);
     field.remove();
+    ev.stopPropagation();
   });
 
   publisher.list('', function (err, resources) {
@@ -188,8 +207,6 @@ function addToot (publisher, data) {
   var entry = $(hentry(data));
   $('#entries').prepend(entry);
   entry.find('time.timeago').timeago();
-
-  injectEntryUI();
 }
 
 function loadToots (publisher) {
@@ -214,8 +231,6 @@ function loadToots (publisher) {
 
     // Make the timestamps all fancy!
     $('time.timeago').timeago();
-
-    injectEntryUI();
   });
 
 }
@@ -295,20 +310,4 @@ function profileEdit () {
 function profileEditDone () {
   elAbout.find('.ui-only').remove();
   elAbout.removeClass('editing');
-}
-
-function injectEntryUI () {
-  $('.h-entry').each(function () {
-    var entry = $(this);
-    var header = entry.find('header');
-
-    var uis = entry.find('.ui-only');
-    if (uis.length) { return; }
-
-    var content = entry.find('.e-content');
-
-    content.after('<button class="ui-only entry-edit btn btn-success btn-sm">Edit</button>');
-    content.after('<button class="ui-only entry-edit-done btn btn-primary btn-sm">Done</button>');
-    content.after('<button class="ui-only entry-delete btn btn-danger btn-sm">Delete</button>');
-  });
 }
